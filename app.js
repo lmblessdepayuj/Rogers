e0c6372e62262c9b125b0efa969aab0db37749d3
lmblessdepayuj <149177889+lmblessdepayuj@users.noreply.github.com>
2023-10-27 07:20:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_uuuu4p2", "template_89r20ed", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
